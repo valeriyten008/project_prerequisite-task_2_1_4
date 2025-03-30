bc3577227e94b59d14576b5bcf3be2960a12d685
Ten Valeriy <walera.tenlist.ru@MacBook-Air-Ten.local>
2025-03-30 23:31:00 +0900

    Initial commit

---
+++ src/main/java/app/model/AnimalsCage.java
@@ -0,0 +1,28 @@
+package app.model;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AnimalsCage {
+
+    private Animal animal1;
+    private Animal animal2;
+    private Timer time;
+
+    @Autowired
+    public AnimalsCage(@Qualifier("cat") Animal animal1, @Qualifier("dog") Animal animal2, Timer time) {
+        this.animal1 = animal1;
+        this.animal2 = animal2;
+        this.time = time;
+    }
+
+    public void whatAnimalSay() {
+        System.out.println("Say:");
+        System.out.println(animal1.toString() +", " + animal2.toString());
+        System.out.println("At:");
+        System.out.println(time.getTime());
+        System.out.println("________________________");
+    }
+}
